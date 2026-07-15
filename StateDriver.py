41caed860525ee440359491c3dfe75fdb4f44cf0
Mikhail Antipin <antipin.mikhail@icloud.com>
2026-07-15 23:28:23 +0800

    Added vibe coded preliminary status driver for unitree sdk

--- StateDriver.py
+++ StateDriver.py
@@ -1,1 +1,19 @@
+from unitree_sdk2py.core.channel import ChannelFactoryInitialize
 from unitree_sdk2py.core.channel import ChannelSubscriber
+
+# The message type depends on the topic
+from unitree_sdk2py.idl.unitree_go.msg.dds_ import SportModeState_
+
+class StateDriver:
+
+    def __init__(self):
+        # Subscribe to robot state topic
+        self.subscriber = ChannelSubscriber(
+            "rt/sportmodestate",
+            SportModeState_
+        )
+
+        self.subscriber.Init()
+
+    def get_state(self):
+        return self.subscriber.Read()
