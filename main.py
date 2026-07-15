e131c37916fe4ebaf7e16ae13f7cd56705fcc596
Mikhail Antipin <antipin.mikhail@icloud.com>
2026-07-15 23:40:37 +0800

    removed n word from main.py

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ print("Loading MiggyOS TM Copyright 2026 All Rights Reserved by The Miggy...")
 
 import sys
 from unitree_sdk2py.core.channel import ChannelFactoryInitialize
-from Miggy import Miggy as Niggy
+from Miggy import Miggy
 
 ChannelFactoryInitialize(0, sys.argv[1])
 print("Connected Succesfully!")
 
-miggy = Niggy()
-commands = {1: "quit"}
+miggy = Miggy()
+commands = {"quit": "Quit the MiggyOS", "move": "Move the robot forward", "rotate": "Rotate the robot"}
+running = True
 
 def list_commands():
     for cmd in commands:
         print(cmd)
+        print(commands[cmd])
 
 running = True
 while running:
