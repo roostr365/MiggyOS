d69b2b2c59a906c6d9d43d1f7c7ae74b0182e2c7
Mikhail Antipin <antipin.mikhail@icloud.com>
2026-07-15 23:25:18 +0800

    Added move and rotate sleep functions temp

--- LocomotionDriver.py
+++ LocomotionDriver.py
@@ -1,6 +1,5 @@
 from unitree_sdk2py.g1.loco.g1_loco_client import LocoClient
 import time
-
 class LocomotionDriver:
 
     def __init__(self, client):
@@ -9,5 +8,13 @@ class LocomotionDriver:
     def stop(self):
         self.client.Move(0, 0, 0)
 
-    def move_dist(self, dist, speed):
+    def move_dist_sleep(self, dist, speed):
         self.client.Move(speed, 0, 0)
+        time.sleep(dist/speed)
+        self.stop()
+
+    def rotate_radians_sleep(self, angle, speed):
+        self.client.Move(0, 0, speed)
+        time.sleep(angle/speed)
+        self.stop()
+    
