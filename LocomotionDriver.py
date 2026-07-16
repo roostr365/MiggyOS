2d864dfa8e9e406adf9e1d6869dc2b8e1bbe41af
Mikhail Antipin <antipin.mikhail@icloud.com>
2026-07-16 14:00:26 +0800

    Added rotate and moev

--- LocomotionDriver.py
+++ LocomotionDriver.py
@@ -3,26 +3,35 @@ import time
 
 class LocomotionDriver:
 
-    def __init__(self, client):
-        self.client = client
+	def __init__(self, client):
+		self.client = client
 
-    def stop(self):
-        self.client.Move(0, 0, 0)
+	def stop(self):
+		self.client.Move(0, 0, 0)
 
-    def move_dist_sleep(self, dist, speed):
-    	if speed == 0:
-    		print("Movement canceled.")
-    		return
-    	self.client.Move(speed, 0, 0, True)
-    	time.sleep(abs(dist/speed))
-    	self.stop()
+	def move_dist_sleep(self, dist, speed):
+		if speed == 0:
+			print("Movement canceled.")
+			return
+		self.client.Move(speed, 0, 0, True)
+		time.sleep(abs(dist/speed))
+		self.stop()
 
-    def rotate_radians_sleep(self, angle, speed):
-    	if speed == 0:
-    		print("Movement canceled.")
-    		return
-    	self.client.Move(0, 0, speed, True)
-    	time.sleep(abs(angle/speed))
-    	self.stop()
-    
+	def rotate_radians_sleep(self, angle, speed):
+		if speed == 0:
+			print("Movement canceled.")
+			return
+		self.client.Move(0, 0, speed, True)
+		time.sleep(abs(angle/speed))
+		self.stop()
+
+	def move_and_rotate(self, move_dist, rotate_angle, move_speed, rotate_speed):
+		if move_speed == 0 or rotate_speed == 0:
+			print("Movement canceled.")
+			return
+		self.client.Move(move_speed, 0, rotate_speed, True)
+		time.sleep(abs(move_dist/move_speed))
+		self.stop()
+
+
  
