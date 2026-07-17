e602dbfe65aac867a0a271a8c5190c0f065294ac
pickle69420 <sun@natureisgod.xyz>
2026-07-16 19:18:50 -0700

    Added back api key

--- AIMiggyController.py
+++ AIMiggyController.py
@@ -1,4 +1,4 @@
-from openai import OpenAI
+from openai import OpenAI, api_key
 from Miggy import Miggy
 import math
 import time
@@ -16,7 +16,7 @@ miggy.move_dist(distance: float, speed: float)
 miggy.rotate_angle(angle: float, speed: float)
     angle: radians (+ counterclockwise, - clockwise)
     speed: radians/second (+ left, - right)
-    
+
 miggy.run_special(str: string)
     str: string that is used by the python sdk to find the high level command to move the arm. Can only be exactly these options:
     shake hand
@@ -28,17 +28,17 @@ miggy.run_special(str: string)
     left kiss
     heart
     right heart
-    hands up 
+    hands up
     x-ray
     right hand up
     reject
     right kiss
     two-hand kiss
-    #End of list 
+    #End of list
     Give the action time to complete.
-    
+
 miggy.release_arm()
-    After using a special arm action, this function moves the arm back to a neutral position. 
+    After using a special arm action, this function moves the arm back to a neutral position.
 
 Available modules:
 - math
@@ -74,11 +74,9 @@ class AIMiggy:
     def __init__(self):
         self.client = OpenAI(
             base_url="https://integrate.api.nvidia.com/v1",
-            api_key=os.getenv("NVAPI_KEY")
+            api_key="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"
         )
-        if not self.client.api_key:
-            raise ValueError("NVAPI_KEY environment variable not set")
-        
+
         self.mode = 0
 
     def askAIMiggy(self, query):
@@ -97,7 +95,7 @@ class AIMiggy:
         code = self.askAIMiggy(query).output_text
         print(code)
         try:
-            # Directly execute generated code; rely on the caller to provide the miggy instance
+            input("code about to be executed: " + code + " Enter to accept")
             exec(code)
         except Exception as e:
             print(str(e) + " Please try again.")
